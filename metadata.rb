ac72a04db78f9effcbbe19e5c1cbb936d41428b2
Marcus Geißler <marcus3006@gmail.com>
2014-08-24 19:00:11 +0200

    berkshelf init

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'Vagrantchef'
+maintainer       'Marcus Geißler'
+maintainer_email 'marcus3006@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures .'
+long_description 'Installs/Configures .'
+version          '0.1.0'
+
